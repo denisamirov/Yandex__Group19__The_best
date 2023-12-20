5c4f0c524d873acc7949f1109b28fe1a45b88b75
Denis Amiro <102862671+denisamirov@users.noreply.github.com>
2023-12-20 20:25:53 +0300

    veb_4_1

---
+++ Module II/Вебинар_4/Example/scripts.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('.container__main');
+const button = document.querySelector('.container__button')
+const body = document.querySelector('body')
+
+const name = document.querySelector('.name')
+const sec_name = document.querySelector('.sec_name')
+
+button.onclick = simplyKey
+
+function simplyKey() {
+    let a = document.createElement('div')
+    a.innerHTML = `<h1>Hello ${name.value}! Привет ${sec_name.value}</h1>`
+    container.appendChild(a)
+}
