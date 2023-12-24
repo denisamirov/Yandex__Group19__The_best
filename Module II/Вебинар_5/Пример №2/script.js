00868b99a177ba28d6dca05e16d5678df4e8a553
Steiden <baldindaniilza@mail.ru>
2023-12-24 22:02:26 +0500

    Реализация удаления

--- Module II/Вебинар_5/Пример №2/script.js
+++ Module II/Вебинар_5/Пример №2/script.js
@@ -7,6 +7,11 @@ let wordsContainer = document.getElementById('wordsContainer');
  for (let i = 0; i < englishWords.length; i++) {
     let wordBlock = document.createElement('div');
     let wordBlockHTML = '<h3>' + englishWords[i] + '</h3>' + '<p>' + russianWords[i] + '</p>';
+	wordBlock.classList.add("wordBlockHTML");
     wordBlock.innerHTML = wordBlockHTML;
     wordsContainer.appendChild(wordBlock);
+
+	wordBlock.addEventListener('click', (e) => {
+		e.currentTarget.remove();
+	})
 }
