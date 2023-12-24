f4ed6b9bb9f23dad2cbdf2c3b9ed8428bfd12580
Steiden <baldindaniilza@mail.ru>
2023-12-24 22:05:58 +0500

    Случайно не создал ветку, вернул как было

--- Module II/Вебинар_5/Пример №2/script.js
+++ Module II/Вебинар_5/Пример №2/script.js
@@ -7,11 +7,6 @@ let wordsContainer = document.getElementById('wordsContainer');
  for (let i = 0; i < englishWords.length; i++) {
     let wordBlock = document.createElement('div');
     let wordBlockHTML = '<h3>' + englishWords[i] + '</h3>' + '<p>' + russianWords[i] + '</p>';
-	wordBlock.classList.add("wordBlockHTML");
     wordBlock.innerHTML = wordBlockHTML;
     wordsContainer.appendChild(wordBlock);
-
-	wordBlock.addEventListener('click', (e) => {
-		e.currentTarget.remove();
-	})
 }
